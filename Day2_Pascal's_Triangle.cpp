ccd00f6224a547630a74450518711171e3290a90
VB-Choudhary <295301211+VB-Choudhary@users.noreply.github.com>
2026-06-21 13:36:29 +0530

    Update Day2_Pascal's_Triangle.cpp
    Updated code using linear row generation approach. Time: O(N^2), Auxiliary Space: O(1).

--- Day2_Pascal's_Triangle.cpp
+++ Day2_Pascal's_Triangle.cpp
@@ -4,17 +4,16 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> final;
         for(int r=1;r<=numRows;r++){
+            int res=1;
             vector<int> ans;
-            for(int c=1;c<=r;c++){
-                int res=1;
-                for(int i=0;i<c-1;i++){
-                    res= res*((r-1)-i);
-                    res = res/(i+1);
-                }
+            ans.push_back(1);
+            for(int c=1;c<r;c++){
+                res=res*(r-c)/c;
                 ans.push_back(res);
             }
             final.push_back(ans);
         }
         return final;
+        
     }
 };
